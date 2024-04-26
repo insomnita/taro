30072433ab0a1762297166ceadb709fc3ce83efb
Дашечка <macintosh@Dasecka.local>
2024-04-27 00:10:06 +0700

    Изменения в файле src/App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
-import {useEffect} from "react";
-const tg = window.Telegram.WebApp
 
-    function App() {
-
-  useEffect(() => {
-    tg.ready();
-  }, []);
-
-  const onClose = () => {
-    tg.close()
-  }
-
-  return (
-      <div className="App">
-        work
-        <button onClick={onClose}>Закрыть</button>
-      </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            work
+        </div>
+    );
 }
-
-export default App;
-
 
 export default App;
